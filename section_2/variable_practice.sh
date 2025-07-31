11ee3c8ad230eacd7d7c598a90fa57627916695a
Bilal <cifci.bilal@outlook.com>
2025-07-31 13:10:57 +0300

    section 2

---
+++ section_2/variable_practice.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+employee="Sarah"
+echo "Dear ${employee}"
+echo "The board and I would like to thank you for the excellent work that you have been doing over the last few months"
+echo "keep up the good work, ${employee}"
+echo "Best wishes,"
+echo "Karen, CEO"
